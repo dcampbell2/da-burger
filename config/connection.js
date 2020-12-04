529c8a8771bb14b4f0a6e099a093517ebd2cbc6d
Dame <dcampbell7095@gmail.com>
2020-12-04 18:52:27 -0500

    making connection to database

--- config/connection.js
+++ config/connection.js
@@ -7,3 +7,11 @@ const connection = mysql.createConnection({
     password: "camcam24",
     database: "burgers_db"
 })
+
+connection.connect((err)=>{
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id: " + connection.threadId)
+})
